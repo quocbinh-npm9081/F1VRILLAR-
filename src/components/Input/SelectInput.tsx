3cc8ad4f70afbc5c3440779a06b5d1b85d832392
louis <quocbinhgithub@gmail.com>
2023-06-22 21:09:13 +0700

    feat: add SelectInput Component

---
+++ src/components/Input/SelectInput.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+type TProp = {
+  title: string;
+  value: string;
+  setValue: (value: string) => void;
+  options: any;
+};
+const SelectInput: React.FC<TProp> = ({ value, setValue, title, options }) => {
+  const handleChange = (event: SelectChangeEvent) => {
+    setValue(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="f1-select-label">{title}</InputLabel>
+        <Select
+          labelId="f1-select-label"
+          id="f1-select"
+          value={value}
+          label={title}
+          onChange={handleChange}
+        >
+          {options.map((opt: any, index: number) => (
+            <MenuItem key={index} value={10}>
+              Ten
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+export default SelectInput;
