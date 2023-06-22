d5e3f72c1d4e19f03a8ff26eb167ecd3ae3243fb
louis <quocbinhgithub@gmail.com>
2023-06-22 19:19:57 +0700

    feat: Complete NavBar page routing component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import ReactRouter from "./config/ReactRouter";
 import MuiThemeProvider from "./config/MuiThemeProvider";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <MuiThemeProvider>
+          <NavBar />
           <ReactRouter />
         </MuiThemeProvider>
       </BrowserRouter>
